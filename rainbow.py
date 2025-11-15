047d152b02aee807a0ea7e16278bf5d585e39f9d
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2025-11-15 09:41:21 -0500

    Small modifications for rainbow

--- rainbow.py
+++ rainbow.py
@@ -2,7 +2,10 @@ import pygame
 
 pygame.init()
 
-size = (800, 600)
+size_x = 800
+size_y = 600
+
+size = (size_x, size_y)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Rainbow")
 
@@ -15,6 +18,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+    pygame.draw.circle(screen, "blue", (size_x // 2, size_y // 2),
+        300) 
     pygame.display.flip()
     clock.tick(60)
 
